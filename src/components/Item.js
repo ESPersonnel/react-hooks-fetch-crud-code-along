da5e7e8f678213b2f3aac83302bb6988058426cc
Wycliffe Ndiba <w.ndiba@gmail.com>
2022-07-19 17:24:50 +0300

    Added gunction to handle add to cart

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,29 @@
 import React from "react";
 
-function Item({ item }) {
+function Item({ item, onUpdateItem }) {
+
+  function handleAddToCartClick() { // Add function to handle add to cart click
+    fetch(`http://localhost:4000/items/${item.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          isInCart: !item.isInCart, // Toggle isInCart
+          }),
+        })
+        .then((res) => res.json())
+        .then((data) => onUpdateItem(data));
+  }
+
   return (
     <li className={item.isInCart ? "in-cart" : ""}>
       <span>{item.name}</span>
       <span className="category">{item.category}</span>
-      <button className={item.isInCart ? "remove" : "add"}>
+      <button 
+        className={item.isInCart ? "remove" : "add"}
+        onClick={handleAddToCartClick}
+      >
         {item.isInCart ? "Remove From" : "Add to"} Cart
       </button>
       <button className="remove">Delete</button>
