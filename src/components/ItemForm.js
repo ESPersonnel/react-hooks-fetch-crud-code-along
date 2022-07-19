444f8f0c5ca7cb6247fb8d54a56bd18ef6f05581
Wycliffe Ndiba <w.ndiba@gmail.com>
2022-07-19 16:43:54 +0300

    Add function to Handle Submissions

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -4,8 +4,27 @@ function ItemForm() {
   const [name, setName] = useState("");
   const [category, setCategory] = useState("Produce");
 
+  // Add function to handle submissions
+  function handleSubmit(e) {
+    e.preventDefault();
+    const itemData = {
+      name: name,
+      category: category,
+      isInCart: false,
+    };
+    fetch("http://localhost:4000/items", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(itemData),
+    })
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+  }
+
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={handleSubmit}>
       <label>
         Name:
         <input
